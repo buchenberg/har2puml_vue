8685d4ce940563e8feda737799678c9c49229fa7
Gregory Buchenberger <buchenberger@gmail.com>
2018-05-02 17:08:31 -0700

    url stuff

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'vue_ace/',
+    assetsPublicPath: 'https://buchenberg.github.io/vue_ace/',
 
     /**
      * Source Maps
